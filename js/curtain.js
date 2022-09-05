8f82b5bf379eadb841b6f1853947fbd3c7046982
Russell Warner <rewarner@russellbits.com>
2022-09-05 11:05:56 -0400

    Converting Jquery selector to Vanilla JS querySelectors.

--- js/curtain.js
+++ js/curtain.js
@@ -6,16 +6,51 @@ $(document).ready(function(){
 	var controller = new ScrollMagic.Controller();
 
 	// Introduction (Cover) elements
-	var raiseCurtain = document.getElementById("bottom-curtain")
-	// TweenLite.to(raiseCurtain, 1.5, {bottom:'0px'});
-	$('#bottom-curtain').transition({ y: '-400px' }, 2600, 'snap')
-	$('#top-left-airliner').transition({ left: 0, top: 0, delay: 800 }, 2600, 'easeOutExpo')
-	$('#top-right-airliner').transition({ right: 0, top: 0, delay: 800 }, 2600, 'easeOutExpo')
-	$('#top-left-bridge').transition({ opacity: 1 }, 1800)
-	$('#top-right-bridge').transition({ opacity: 1 }, 1800)
-	$('h1:nth-child(1)').transition({ opacity: 1, delay:1000 }, 1500)
-	$('h1:nth-child(2)').transition({ opacity: 1, delay:1600 }, 1500)
-	$('h1:nth-child(3)').transition({ opacity: 1, delay:2200 }, 1500)
+	const bottomCurtain = document.getElementById('bottom-curtain')
+	const topLeftAirliner = document.getElementById('top-left-airliner')
+	const topRightAirliner = document.getElementById('top-right-airliner')
+	const topLeftBridge = document.getElementById('top-left-bridge')
+	const topRightBridge = document.getElementById('top-right-bridge')
+
+	bottomCurtain.style.transform = 'translateY(-400px)'
+	bottomCurtain.style.transition = 'all 2600ms snap'
+
+	topLeftAirliner.style.transform = 'translate(0, 0)'
+	topLeftAirliner.style.transition = 'all 800ms easeOutExpo'
+
+	topRightAirliner.style.transform = 'translate(0, 0)'
+	topRightAirliner.style.transition = 'all 800ms easeOutExpo'
+
+	topLeftBridge.style.opacity = 0
+	topRightBridge.style.opacity = 0
+
+	topLeftBridge.style.transition = 'opacity 800ms easeOutExpo'
+	topRightBridge.style.transition = 'opacity 800ms easeOutExpo'
+
+	const titleLine1 = document.querySelector('h1:nth-child(1)')
+	const titleLine2 = document.querySelector('h1:nth-child(2)')
+	const titleLine3 = document.querySelector('h1:nth-child(3)')
+
+	titleLine1.style.opacity = 0
+	titleLine2.style.opacity = 0
+	titleLine3.style.opacity = 0
+
+	titleLine1.style.transition = 'opacity 800ms easeOutExpo 800ms'
+	titleLine2.style.transition = 'opacity 800ms easeOutExpo 1200ms'
+	titleLine3.style.transition = 'opacity 800ms easeOutExpo 1600ms'
+	
+	setTimeout(() => {
+    // topLeftBridge.style.opacity = 1;
+		// topRightBridge.style.opacity = 0
+	}, 800);
+
+	/// $('#top-left-airliner').transition({ left: 0, top: 0, delay: 800 }, 2600, 'easeOutExpo')
+	// $('#top-right-airliner').transition({ right: 0, top: 0, delay: 800 }, 2600, 'easeOutExpo')
+	// $('#top-left-bridge').transition({ opacity: 1 }, 1800)
+	// $('#top-right-bridge').transition({ opacity: 1 }, 1800)
+	// $('h1:nth-child(1)').transition({ opacity: 1, delay:1000 }, 1500)
+	// $('h1:nth-child(2)').transition({ opacity: 1, delay:1600 }, 1500)
+	// $('h1:nth-child(3)').transition({ opacity: 1, delay:2200 }, 1500)
 
 	var startTimeline = new TimelineMax();
 	var shrinkTopLCorner = TweenMax.to('#top-left-airliner', 0.2, { scale:'.8', top:'-25', left: '-25' })
